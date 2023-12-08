ce6e28f944397d729b1355d40cf8bdbd75c3acff
mj <mikulej7@gmail.com>
2023-12-08 17:07:44 +0100

    New Blur, Better Performence Tests

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -6,29 +6,30 @@ use std::fs;
 
 fn main() {
    
-        let args: Vec<String> = std::env::args().collect();
-        
-        //1. Get images' paths
-        let paths = fs::read_dir("./images").unwrap();
+    let args: Vec<String> = std::env::args().collect();
     
-        let mut image_paths = Vec::new();
-    
-        for path in paths {
-            let path_str = path.unwrap().path().display().to_string();
-            if path_str.ends_with(".png") || path_str.ends_with(".jpg") {
-                image_paths.push(path_str);
-            }
+    //1. Get images' paths
+    let paths = fs::read_dir("./images").unwrap();
+
+    let mut image_paths = Vec::new();
+
+    for path in paths {
+        let path_str = path.unwrap().path().display().to_string();
+        if path_str.ends_with(".jpg") {
+            image_paths.push(path_str);
         }
-    
+    }
+   
         //2.Load Image
         let img = load_image(&image_paths[0]);
         let (width, height) = img.dimensions(); // Obtain the image's width and height.
         let mut imgbuf = image::ImageBuffer::new(width, height);
         //3. Manipulate image
+    
         let mode = args[1].as_str();
             match mode {
                 "r" => {//removal
-                    imgbuf = img.to_rgba8();
+                    imgbuf = img.to_rgba8();           
                     for i in (2..args.len()).step_by(4) {
                         if let Some(arg1) = args.get(i) {
                             if let Some(arg2) = args.get(i + 1) {
@@ -68,15 +69,16 @@ fn main() {
                             } 
                         } 
                     }
+                
                     imgbuf.save("out/out.png").unwrap();
                 }, 
                 "m" => { //merge
                     let ratio1 = args[2].parse::<f32>().unwrap();
                     let ratio2 = 1.0 - ratio1;
                     let mut img2 = load_image(&image_paths[1]);
-                    img2 = img2.resize(width, height, image::imageops::FilterType::Nearest);  
-                   
+                    let (width2, height2) = img2.dimensions();                    
                     for(x, y, pixel1) in img.pixels() {
+                        if x >= width2 || y >= height2 {continue;} //if the pixel is out of bounds, skip it
                         let mut pixel2 = img2.get_pixel(x, y);
                         let mut mergedPixel: image::Rgba<u8> = image::Rgba([
                             ((pixel1[0] as f32 * ratio1) + (pixel2[0] as f32 * ratio2)) as u8,
@@ -85,20 +87,82 @@ fn main() {
                             ((pixel1[3] as f32 * ratio1) + (pixel2[3] as f32 * ratio2)) as u8
                             ]);
                         imgbuf.put_pixel(x, y, mergedPixel);
-                    }
+                    } 
                     imgbuf.save("out/out.png").unwrap();
                 }, 
                 "b" => {//blur
-                    let blurStrength = args[2].parse::<f32>().unwrap();
-                    imgbuf = img.blur(blurStrength).to_rgba8();
-                    imgbuf.save("out/out.png").unwrap();         
+                    for(x, y,pixel) in img.pixels(){
+                        if x <= 1 || y <= 1 || x >= width-2 || y >= height-2{continue;}
+                        let nearbyPixels = [
+                            img.get_pixel(x-2, y-2),
+                            img.get_pixel(x-2, y-1),
+                            img.get_pixel(x-2, y),
+                            img.get_pixel(x-2, y+1),
+                            img.get_pixel(x-2, y+2),
+                            img.get_pixel(x-1, y-2),
+                            img.get_pixel(x-1, y-1),
+                            img.get_pixel(x-1, y),
+                            img.get_pixel(x-1, y+1),
+                            img.get_pixel(x-1, y+2),
+                            img.get_pixel(x, y-2),
+                            img.get_pixel(x, y-1),
+                            pixel,
+                            img.get_pixel(x, y+1),
+                            img.get_pixel(x, y+2),
+                            img.get_pixel(x+1, y-2),
+                            img.get_pixel(x+1, y-1),
+                            img.get_pixel(x+1, y),
+                            img.get_pixel(x+1, y+1),
+                            img.get_pixel(x+1, y+2),
+                            img.get_pixel(x+2, y-2),
+                            img.get_pixel(x+2, y-1),
+                            img.get_pixel(x+2, y),
+                            img.get_pixel(x+2, y+1),
+                            img.get_pixel(x+2, y+2)
+                        ];
+                        let mut blurredPixel: image::Rgba<u8> = pixel;
+
+                        for i in 0..3{
+                            blurredPixel[i] = (
+                                nearbyPixels[0][i] as f32 * 0.00390625 + //1
+                                nearbyPixels[1][i] as f32 * 0.015625 +   //4
+                                nearbyPixels[2][i] as f32 * 0.0234375 +   //6
+                                nearbyPixels[3][i] as f32 * 0.015625 +   //4  
+                                nearbyPixels[4][i] as f32 * 0.00390625 +//1
+                                nearbyPixels[5][i] as f32 * 0.015625 +//4
+                                nearbyPixels[6][i] as f32 * 0.0625 +//16
+                                nearbyPixels[7][i] as f32 * 0.09375 +//24
+                                nearbyPixels[8][i] as f32 * 0.0625 +//16
+                                nearbyPixels[9][i] as f32 * 0.015625 +//4
+                                nearbyPixels[10][i] as f32 * 0.0234375 +//6
+                                nearbyPixels[11][i] as f32 * 0.09375 +//24
+                                nearbyPixels[12][i] as f32 * 0.140625 +//36
+                                nearbyPixels[13][i] as f32 * 0.09375 +//24
+                                nearbyPixels[14][i] as f32 * 0.0234375 +//6
+                                nearbyPixels[15][i] as f32 * 0.015625 +//4
+                                nearbyPixels[16][i] as f32 * 0.0625 +//16
+                                nearbyPixels[17][i] as f32 * 0.09375 +//24
+                                nearbyPixels[18][i] as f32 * 0.0625 +//16
+                                nearbyPixels[19][i] as f32 * 0.015625 +//4
+                                nearbyPixels[20][i] as f32 * 0.00390625 +//1
+                                nearbyPixels[21][i] as f32 * 0.015625 +//4
+                                nearbyPixels[22][i] as f32 * 0.0234375 +//6
+                                nearbyPixels[23][i] as f32 * 0.015625 +//4
+                                nearbyPixels[24][i] as f32 * 0.00390625//1
+                            ) as u8;
+                        }
+                        imgbuf.put_pixel(x, y, blurredPixel);
+                    }   
+              
+                    imgbuf.save("out/out.png").unwrap();      
+                    
                 }, 
                 _ => panic!("Invalid operation. Expected 'r', 'm', or 'b'. Was {}", mode),
             }
     //TESTS
     // let mut total_time = std::time::Duration::new(0, 0);
     // for _ in 0..12{
-    //             let start = std::time::Instant::now();
+    // let start = std::time::Instant::now();
     //     let end = std::time::Instant::now();
     //     let elapsed_time = end.duration_since(start);
     //     total_time += elapsed_time;
